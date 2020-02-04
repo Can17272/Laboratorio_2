e91c89fe887d2a37f6ff0ded3609fbabffbf1631
Can17272 <can17272@uvg.edu.gt>
2020-02-04 00:15:12 -0600

    Setup

---
+++ Lab2.c
@@ -0,0 +1,28 @@
+/* 
+ * File:   Lab2.c
+ * Author: Daniel Cano; 17272
+ *
+ * Created on February 4, 2020, 12:06 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+/*
+ * 
+ */
+
+void setup (void){
+    PORTA=0;
+    PORTB=0;
+    PORTC=0;
+    PORTD=0;
+    ANSEL=0;
+    ANSELH=0;
+}
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
