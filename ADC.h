03168060f80bfce5236abd9a1a03060dccd0617d
Can17272 <can17272@uvg.edu.gt>
2020-02-05 15:12:24 -0600

    Configuración Interrupciones y ADC

--- ADC.h
+++ ADC.h
@@ -19,5 +19,25 @@ extern "C" {
 }
 #endif
 
+void int_con(void){
+        INTCONbits.GIE=1;
+        INTCONbits.INTE=1;
+        INTCONbits.PEIE=1;
+        INTCONbits.T0IE=1;
+        INTCONbits.T0IF=0; //Interrupciones del INTCON
+        PIE1bits.ADIE=1;  //Enable de las interrupciones
+
+}
+void (ADC){
+        ADCON0bits.ADCS0=1;
+        ADCON0bits.ADCS1=0;
+        ADCON0bits.CHS=0;
+        ADCON0bits.ADON=1; 
+        
+        ADCON1bits.ADFM=0;
+        ADCON1bits.VCFG0=0;  
+        ADCON1bits.VCFG1=0; // ADC configuration
+
+}
 #endif	/* ADC_H */
 
