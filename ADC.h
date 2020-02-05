51235adfe8d116b109c367082e448fc742673284
Can17272 <can17272@uvg.edu.gt>
2020-02-05 15:03:48 -0600

    Header ADC

---
+++ ADC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ADC.h
+ * Author: TOSHIBA
+ *
+ * Created on February 5, 2020, 3:02 PM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC_H */
+
