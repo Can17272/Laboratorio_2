3030e22ff39b6171d9da1996d04e7a640f31b737
Can17272 <can17272@uvg.edu.gt>
2020-02-05 15:01:42 -0600

    Contador Binario e interrupción antirrebote

--- Lab2.c
+++ Lab2.c
@@ -10,7 +10,8 @@
 #include <xc.h>
 
 int CONT_BIN;
-
+int AR1;
+int AR2;
 
 void setup (void){
     PORTA=0;
@@ -26,13 +27,43 @@ void setup (void){
     TRISD=0;
 }
 
+void __interrupt() ISR (void){
+    /*if (PIR1bits.ADIF==1){
+        PORTD = CONT_BIN;
+        PIR1bits.ADIF=0;
+    }*/
+    if (INTCONbits.RBIF==1 && PORTBbits.RB0 ==1){
+        AR1=1;
+        INTCONbits.RBIF=0;
+    
+    }
+    if (INTCONbits.RBIF==1 && PORTBbits.RB1 ==1){
+        AR2=1;
+        INTCONbits.RBIF=0;
+    }
+}
+
 void main (void){
     setup();
     CONT_BIN = 0;
+    AR1=0;
+    AR2=0;
+    CONT_BIN = 0;
+       
     while (1){
-        if
+        if (PORTBbits.RB0 ==0 && AR1=1){
+            CONT_BIN++;
+            PORTD = CONT_BIN;
+            AR1=0;
+           
+        }
+        if (PORTBbits.RB1 ==0 && AR2=1){
+            CONT_BIN--;
+            PORTD = CONT_BIN;
+            AR2=0;
+        }
+            
     
     }
            
 }
-
