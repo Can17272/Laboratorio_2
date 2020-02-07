3fd91c2e6ec59839f805aebe746e352c68fe76be
Can17272 <can17272@uvg.edu.gt>
2020-02-07 10:37:20 -0600

    Comentarios

--- Laboratorio 2.X/ADC.c
+++ Laboratorio 2.X/ADC.c
@@ -7,7 +7,7 @@ void ADC(ADC_VALUE){
         ADCON0bits.ADCS0=1;
         ADCON0bits.ADCS1=0;
         ADCON0bits.CHS=0;
-        ADCON0bits.ADON=1;
+        ADCON0bits.ADON=1; //Inicialización del ADC
             
         break;
     
--- Laboratorio 2.X/Lab22.c
+++ Laboratorio 2.X/Lab22.c
@@ -36,7 +36,7 @@ void setup (void){
 void __interrupt() ISR (void){
     if (PIR1bits.ADIF==1){
         PORTD = CONT_BIN;
-        PIR1bits.ADIF=0;
+        PIR1bits.ADIF=0; //Interrupción del ADC
     }
     if (INTCONbits.RBIF==1 && PORTBbits.RB0 ==1){
         AR1=1;
@@ -45,12 +45,13 @@ void __interrupt() ISR (void){
     }
     if (INTCONbits.RBIF==1 && PORTBbits.RB1 ==1){
         AR2=1;
-        INTCONbits.RBIF=0;
+        INTCONbits.RBIF=0; //Interrupciones del puerto B para antirrebote
     }
 }
 
 void main (void){
     setup();
+    ADC();
     CONT_BIN = 0;
     AR1=0;
     AR2=0;
@@ -60,19 +61,19 @@ void main (void){
         if (PORTBbits.RB0 ==0 && AR1==1){
             CONT_BIN++;
             //PORTD = CONT_BIN;
-            AR1=0;
+            AR1=0; //Suma binaria del puerto D
            
         }
         if (PORTBbits.RB1 ==0 && AR2==1){
             CONT_BIN--;
            // PORTD = CONT_BIN;
-            AR2=0;
+            AR2=0; //Resta binaria del puerto D
         }
         if (ADRESH <= CONT_BIN){
-            PORTAbits.RA0 =1;
+            PORTAbits.RA0 =1; //Activación de la alarma
         }
         if (CONT_BIN <= ADRESH){
-            PORTAbits.RA0 =0;
+            PORTAbits.RA0 =0; //Comparación entre el valor del ADC y el puerto D
         }
             
     
